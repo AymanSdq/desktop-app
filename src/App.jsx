f6784daff048eb80a99cfa2ebdae426d7042fe73
AymanSdq <sedqiayman02@gmail.com>
2024-08-20 09:19:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <section className="container-page">
+
+          <h1>
+            First Application for desktop
+          </h1>
+
+      </section>
+    
+    </>
+  );
+}
+
+export default App;
